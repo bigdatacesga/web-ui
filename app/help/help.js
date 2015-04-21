ce84751b29e362eee6913a41b0bbc6998893bca9
albertoesmp <albertoesmp@bitbucket.org>
2015-04-21 15:56:23 +0200

    Help/Documentation has been simplified

--- app/help/help.js
+++ app/help/help.js
@@ -3,8 +3,6 @@
  * @ngdoc function
  * @name hadoopApp.help:HelpCtrl
  * @description 
- * # HelpCtrl
- * Controller of the help view 
  * Allows to see help/documentation
  */
 angular.module('hadoopApp.help', ['ui.router'])
@@ -13,44 +11,5 @@ angular.module('hadoopApp.help', ['ui.router'])
   $stateProvider.state('help', {
     url:'/help',
     templateUrl: 'help/help.html',
-    controller: 'HelpCtrl',
-    controllerAs: 'help'
   });
 }])
-
-.controller('HelpCtrl', [function() {
-  // TODO
-  this.active = "help";
-  this.helpText = helpTextHelp();
-  
-  this.isActive = function(str){
-    if(this.active==str)
-      return "active";
-    return "inactive";
-  };
-  
-  this.setActive = function(str){
-    this.active = str;
-    this.setHelpText();
-  };
-  
-  this.setHelpText = function(){
-    // helpText are defined in assets/js/help_texts.js
-    switch(this.active){
-      case "help":
-	this.helpText = helpTextHelp();
-	break;
-      case "clusters":
-	this.helpText = helpTextClusters();
-	break;
-      case "sshkeys":
-	this.helpText = helpTextSSHKeys();
-	break;
-      case "firewall":
-	this.helpText = helpTextFirewall();
-	break;
-    }
-    
-  }
-}]);
-  
