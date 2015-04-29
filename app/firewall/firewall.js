b6cf81239bdd0dc7ffd71bb99ae570cf43603025
albertoesmp <albertoesmp@bitbucket.org>
2015-04-29 15:51:30 +0200

    Updating configuration components to communicate through services

--- app/firewall/firewall.js
+++ app/firewall/firewall.js
@@ -7,7 +7,7 @@
  * Controller of the firewall view 
  * Allows to see current firewall configuration and to add new rules
  */
-angular.module('hadoopApp.firewall', ['ui.router', 'hadoopApp.rule'])
+angular.module('hadoopApp.firewall', ['ui.router', 'hadoopApp.rule', 'hadoopApp.service.ips'])
 
 .config(['$stateProvider', function ($stateProvider) {
   $stateProvider.state('firewall', {
@@ -18,8 +18,8 @@ angular.module('hadoopApp.firewall', ['ui.router', 'hadoopApp.rule'])
   });
 }])
 
-.controller('FirewallCtrl', [function() {
-  this.ips = [
+.controller('FirewallCtrl', ['IpService', function(IpService) {
+  this.testIps = [
       {
 	ip:'192.168.1.135',
 	mask:'32',
@@ -44,6 +44,22 @@ angular.module('hadoopApp.firewall', ['ui.router', 'hadoopApp.rule'])
       }
     ];
 
+    this.all = IpService.getAll();
+    if(this.all.ips != null){
+      this.allIps = [];
+      for(var i=0 ; i < this.all.ips.length ; i++)
+	this.allIps.push({ip:parseIPFrom(this.all.ips[i].IP),
+	  mask:parseMaskFrom(this.all.ips[i].IP),
+	  //state:this.all.ips.enabled // enabled is not implemented yet
+	  state: 'enabled'
+	});
+      this.ips=this.allIps;
+    }
+    else{
+      alert('Used test ips because communication with API failed\n');
+      this.ips=this.testIps;
+    }
+    
   this.addRule = function(str) {
     // Adds the rule inside text input if it is valid
     if(isValidIP(document.getElementById("firewall_input_rule").value)){
--- app/sshkeys/sshkeys.js
+++ app/sshkeys/sshkeys.js
@@ -7,7 +7,7 @@
  * Controller of the sshkeys view 
  * Allows to see registered ssh keys and to add new keys
  */
-angular.module('hadoopApp.sshkeys', ['ui.router', 'hadoopApp.sshkey'])
+angular.module('hadoopApp.sshkeys', ['ui.router', 'hadoopApp.sshkey', 'hadoopApp.service.keys'])
 
 .config(['$stateProvider', function ($stateProvider) {
   $stateProvider.state('sshkeys', {
@@ -18,14 +18,27 @@ angular.module('hadoopApp.sshkeys', ['ui.router', 'hadoopApp.sshkey'])
   });
 }])
 
-.controller('SSHKeysCtrl', [function() {
-  this.sshKeys = [
-      {key:"ssh-dss AAAAB3NzaC1kc3MAAACBAKfmTAYNrwWKJBmnSl0lN8nNVAb6n7i7Ctj7SCg2e8+Je/Y8mvC494nAwH8UZqCStF5UW8XXMiEt9ax5VXjHLAtqlhdCtRaXDctJiCyVgSUWZRI8AQq+d8bVZ+8jG2vQV0c3Tp/jAE177fpskkVTiBF92r1o/5q3Br6Gy0cRBA4BAAAAFQC4n2JCi75HN30Hca5nnE0Dm6oOKwAAAIB6+PCcBNy+hg9BBrSHUnnPmdQj12k5QSo1GOH3yw2430x0oYIaQjTR3KF5ViPRX1IOq5Z/LlHRkQQclP8Zde8dGLKNNEJBd8w3RYMhqfvxBZWSjAfJKCf1AytjVaTWnEiVVKE2Qv3oXArTYKmf5ly0AtON5BiEJZA/hh7NPqc3zwAAAIBDDTi1bebjDcU3GM+qPZvEX6eZX0JPNElZd5PlGaSctfSwhxIzUYH4MsgLCtyOmG6Bc6KR7aV0wGQREupCnr+E8R3lYeeT4jMmDXp/WK4Raxwpth4p+imw8bondUEgUs+aGR4d2s7fWGuzHo1fd7noHGlTNl0okd5mJG88sXs9hA=="},
-      {key:"ssh-dss AAAAB3NzaC1kc3MAAACBAPJ1HtI5aYP47iyMJVFgnRvx2uq3ruPlXvvw16jSFT/NsKwX6H9yvBpsU6FW5YBpdm3sLgcv+SBBWa5KWQL2KS32qDRQ3zuwIVuV1uc7vUK0GnVyxBrRVoiL5DOfvsQ1EPeF3J2mkzeSg9kJtvm8s6vm6dTB4rDF3R80lrY68AX1AAAAFQDD7JihDo5syG6LGYqyztEoG0c9nQAAAIAqnp4p3vwSwHtJcyN/9VhG4rfcDWLXMlaRhCWhv1P3exD/5joosKUwY82jjaHXuS7iDJkgKjMbufdunxSwZ+FOSdabb29TejJ6Rso+uk50aYe/rRKxn0opsRwbmF9K84AFZ437fBE4VUZSJmtdN9RE8CJNZhzkgzulwkwH7y0ZFwAAAIEAgbwDQ5WWVuDNoG/tYPQQP29ll8uiKHMeEYrlQNhph1BqhN/0s3bSIvRuMRMYtBxv4M6oVJnnOrAyYnSQxyV23lU+UgLuXo56ZxjNaO8AjwyC7K3uZc8AwrawAKfhB/mQTXi7QtqvDk0MdjMgMreBcq3NyzOkAmQRbu2AKPn20RI="},
-      {key:"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEdt4zwzrzGOU6Cz/19RDPU5/e9DO8e+/vb67+zJ8QeODDizDFodEMCY5u/f4XRTYEUy+kzE0PgkEjRPBwghv+WlX1wtmyK/7WwtHLtOKjiUdNr8n7OOAw5KaJFZQ9xToPnRVmR0vRhhk1ckpSlLN9FZvEjbRorAID2qTAQvUUqiU3T/S0O5hdoUzQwImWbbAkN7E4xngWjFjLL2CmXLEjWkRUxHFRxr89H8eB3jyJPN0rIwCReIelb7eDMinx+Hk7s5Jxc7x4pVmKo1o2v8Q8E9uhOntVMMKFDfIQ+TO68hsUXWT+H+nBfXpY8GIQWVMRR2LyRt4O7eDdkVzdYyst"},
-      {key:"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxjG3bewY2Steuz3t1E0dT5qh4KiDl7S7KLV7zbY8/24ST3GJnRiwE0bR8ziPW6JRIMWcsx9aVrQmpDmtvtkIdj6TeLBg6UDxl5NH7QStIjP5N/jHTCt74yuWqI5Y1pTBeSKXuEhe6/yHvoKIuzhM7yw/dR5/hxJnRwmoBsam/BK0DBEQ4ItNWaIiqAmJYoFz46A3flz/w3xZCZlOc3MAYxxvAXuYrGym6VKtxgESo2w5HzKfJPIabbP/Ux32yMFkNw8/4csKZB3n4LmgfvwVuGdJzxeOyvGbDojQquyfd/M9631+DA2uWgvYUSljURrtT9IBcoEowX5wlca7qEyoJ"}
-    ];
+.controller('SSHKeysCtrl', ['KeyService', function(KeyService) {
+  this.testKeys = [
+	  {key:"ssh-dss AAAAB3NzaC1kc3MAAACBAKfmTAYNrwWKJBmnSl0lN8nNVAb6n7i7Ctj7SCg2e8+Je/Y8mvC494nAwH8UZqCStF5UW8XXMiEt9ax5VXjHLAtqlhdCtRaXDctJiCyVgSUWZRI8AQq+d8bVZ+8jG2vQV0c3Tp/jAE177fpskkVTiBF92r1o/5q3Br6Gy0cRBA4BAAAAFQC4n2JCi75HN30Hca5nnE0Dm6oOKwAAAIB6+PCcBNy+hg9BBrSHUnnPmdQj12k5QSo1GOH3yw2430x0oYIaQjTR3KF5ViPRX1IOq5Z/LlHRkQQclP8Zde8dGLKNNEJBd8w3RYMhqfvxBZWSjAfJKCf1AytjVaTWnEiVVKE2Qv3oXArTYKmf5ly0AtON5BiEJZA/hh7NPqc3zwAAAIBDDTi1bebjDcU3GM+qPZvEX6eZX0JPNElZd5PlGaSctfSwhxIzUYH4MsgLCtyOmG6Bc6KR7aV0wGQREupCnr+E8R3lYeeT4jMmDXp/WK4Raxwpth4p+imw8bondUEgUs+aGR4d2s7fWGuzHo1fd7noHGlTNl0okd5mJG88sXs9hA=="},
+	  {key:"ssh-dss AAAAB3NzaC1kc3MAAACBAPJ1HtI5aYP47iyMJVFgnRvx2uq3ruPlXvvw16jSFT/NsKwX6H9yvBpsU6FW5YBpdm3sLgcv+SBBWa5KWQL2KS32qDRQ3zuwIVuV1uc7vUK0GnVyxBrRVoiL5DOfvsQ1EPeF3J2mkzeSg9kJtvm8s6vm6dTB4rDF3R80lrY68AX1AAAAFQDD7JihDo5syG6LGYqyztEoG0c9nQAAAIAqnp4p3vwSwHtJcyN/9VhG4rfcDWLXMlaRhCWhv1P3exD/5joosKUwY82jjaHXuS7iDJkgKjMbufdunxSwZ+FOSdabb29TejJ6Rso+uk50aYe/rRKxn0opsRwbmF9K84AFZ437fBE4VUZSJmtdN9RE8CJNZhzkgzulwkwH7y0ZFwAAAIEAgbwDQ5WWVuDNoG/tYPQQP29ll8uiKHMeEYrlQNhph1BqhN/0s3bSIvRuMRMYtBxv4M6oVJnnOrAyYnSQxyV23lU+UgLuXo56ZxjNaO8AjwyC7K3uZc8AwrawAKfhB/mQTXi7QtqvDk0MdjMgMreBcq3NyzOkAmQRbu2AKPn20RI="},
+	  {key:"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEdt4zwzrzGOU6Cz/19RDPU5/e9DO8e+/vb67+zJ8QeODDizDFodEMCY5u/f4XRTYEUy+kzE0PgkEjRPBwghv+WlX1wtmyK/7WwtHLtOKjiUdNr8n7OOAw5KaJFZQ9xToPnRVmR0vRhhk1ckpSlLN9FZvEjbRorAID2qTAQvUUqiU3T/S0O5hdoUzQwImWbbAkN7E4xngWjFjLL2CmXLEjWkRUxHFRxr89H8eB3jyJPN0rIwCReIelb7eDMinx+Hk7s5Jxc7x4pVmKo1o2v8Q8E9uhOntVMMKFDfIQ+TO68hsUXWT+H+nBfXpY8GIQWVMRR2LyRt4O7eDdkVzdYyst"},
+	  {key:"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxjG3bewY2Steuz3t1E0dT5qh4KiDl7S7KLV7zbY8/24ST3GJnRiwE0bR8ziPW6JRIMWcsx9aVrQmpDmtvtkIdj6TeLBg6UDxl5NH7QStIjP5N/jHTCt74yuWqI5Y1pTBeSKXuEhe6/yHvoKIuzhM7yw/dR5/hxJnRwmoBsam/BK0DBEQ4ItNWaIiqAmJYoFz46A3flz/w3xZCZlOc3MAYxxvAXuYrGym6VKtxgESo2w5HzKfJPIabbP/Ux32yMFkNw8/4csKZB3n4LmgfvwVuGdJzxeOyvGbDojQquyfd/M9631+DA2uWgvYUSljURrtT9IBcoEowX5wlca7qEyoJ"}
+	];
+  
+  this.all = KeyService.getAll();
+  if(this.all.keys!=null){
+    this.allKeys = [];
+    for(var i = 0 ; i < this.all.keys.length ; i++)
+      this.allKeys.push({key:this.all.keys[i].key});
+    this.sshKeys = this.allKeys;
+  }
+  else{
+    alert('Used test keys because communication with API failed\n');
+    this.sshKeys = this.testKeys;
+  }
 
+  
   this.addKey = function() {
     // Adds the key inside text input
     if(isValidSSHKey(document.getElementById("sshkeys_input_sshkey").value)){
