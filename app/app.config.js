7b46f8b64515792456c6173bf55ba9b7b11fc00e
Javier Cacheiro <jlopez@cesga.es>
2016-10-03 10:42:43 +0200

    Rename HDP to Hadoop

--- app/app.config.js
+++ app/app.config.js
@@ -6,7 +6,7 @@
     .module('bigdata')
     .config(['$stateProvider','$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
       // For any unmatched url, redirect to /dashboard
-      $urlRouterProvider.otherwise('/dashboard');
+      $urlRouterProvider.otherwise('/hdp');
     }])
     .run(['$rootScope', '$state', '$log', function($rootScope, $state, $log) {
       $rootScope.$on('unauthorized', function(event, data) {
